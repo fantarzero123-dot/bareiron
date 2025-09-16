61f7f010f61e9d39f8ca75c9d7d550d05bde3b82
Fantarzero <fantarzero123@gmail.com>
2025-09-16 15:00:53 +0700

    Delete include directory

--- include/crafting.h
+++
@@ -1,9 +0,0 @@
-#ifndef CRAFTING_H
-#define CRAFTING_H
-
-#include "globals.h"
-
-void getCraftingOutput (PlayerData *player, uint8_t *count, uint16_t *item);
-void getSmeltingOutput (PlayerData *player);
-
-#endif
--- include/structures.h
+++
@@ -1,6 +0,0 @@
-#ifndef H_STRUCTURES
-#define H_STRUCTURES
-
-void placeTreeStructure (short x, uint8_t y, short z);
-
-#endif
